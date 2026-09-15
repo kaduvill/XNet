3c71618e91a65b14115b03157f7335cfab13b850
kaduvill <jottafanftw@gmail.com>
2026-09-15 09:20:17 +0200

    Improve filter count readability

--- src/main/java/mcjty/xnet/blocks/controller/gui/BlockRenderFilter.java
+++ src/main/java/mcjty/xnet/blocks/controller/gui/BlockRenderFilter.java
@@ -5,6 +5,7 @@ import mcjty.lib.gui.widgets.BlockRender;
 import mcjty.lib.gui.widgets.Widget;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.item.ItemStack;
 
 import java.util.function.Consumer;
@@ -80,19 +81,79 @@ public class BlockRenderFilter extends BlockRender
         float previousZLevel = mc.getRenderItem().zLevel;
         try
         {
-            RenderHelper.renderObject(
-                    mc,
-                    mc.getRenderItem(),
-                    x + bounds.x + getOffsetX(),
-                    y + bounds.y + getOffsetY(),
-                    renderItem,
-                    false,
-                    100.0F
-            );
+            int renderX = x + bounds.x + getOffsetX();
+            int renderY = y + bounds.y + getOffsetY();
+
+            if (renderItem instanceof ItemStack && ((ItemStack) renderItem).getCount() > 1)
+            {
+                ItemStack stack = (ItemStack) renderItem;
+                mc.getRenderItem().zLevel = 100.0F;
+
+                // Render the item, durability and cooldown without McJtyLib's count.
+                RenderHelper.renderItemStack(mc, mc.getRenderItem(), stack, renderX, renderY, "", false);
+                renderFilterCount(stack.getCount(), renderX, renderY);
+            }
+            else {
+                RenderHelper.renderObject(mc, mc.getRenderItem(), renderX, renderY, renderItem, false, 100.0F);
+            }
         }
         finally
         {
             mc.getRenderItem().zLevel = previousZLevel;
         }
+    }
+
+    private void renderFilterCount(int count, int x, int y)
+    {
+        String amount;
+        if (count < 100000)
+        {amount = String.valueOf(count);}
+        else if (count < 1000000)
+        {amount = String.valueOf(count / 1000) + "k";}
+        else if (count < 1000000000)
+        {amount = String.valueOf(count / 1000000) + "m";}
+        else
+        {amount = String.valueOf(count / 1000000000) + "g";}
+
+        int scaled = amount.length() - 2;
+
+        GlStateManager.disableLighting();
+        GlStateManager.disableDepth();
+        GlStateManager.disableBlend();
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(0.0F, 0.0F, 32.0F);
+
+        if (scaled >= 2)
+        {
+            GlStateManager.pushMatrix();
+            GlStateManager.scale(.5F, .5F, .5F);
+            mc.fontRenderer.drawStringWithShadow(
+                    amount,
+                    (x + 17) * 2 - 3 - mc.fontRenderer.getStringWidth(amount),
+                    y * 2 + 22,
+                    16777215
+            );
+            GlStateManager.popMatrix();
+        }
+        else if (scaled == 1)
+        {
+            GlStateManager.pushMatrix();
+            GlStateManager.scale(.75F, .75F, .75F);
+            mc.fontRenderer.drawStringWithShadow(
+                    amount,
+                    (x - 2) * 1.34F + 22.66F - mc.fontRenderer.getStringWidth(amount),
+                    y * 1.34F + 12.66F,
+                    16777215
+            );
+            GlStateManager.popMatrix();
+        }
+        else
+        {
+            mc.fontRenderer.drawStringWithShadow(amount, x + 16 - mc.fontRenderer.getStringWidth(amount), y + 8, 16777215);
+        }
+
+        GlStateManager.popMatrix();
+        GlStateManager.enableDepth();
+        GlStateManager.enableBlend();
     }
 }
